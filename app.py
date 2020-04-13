cd19ee77c49900089983d70fb39bf54a560a0804
Martins Silins <kydzoster@gmail.com>
2020-04-13 14:38:35 +0000

    created app.py first app.route to meaning.html

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'symbols'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@my1stcluster-phyn3.mongodb.net/symbols?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_meaning')
+def get_meaning():
+    return render_template("meaning.html", meaning=mongo.db.meaning.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
