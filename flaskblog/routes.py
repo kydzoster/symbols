f778634be2744b00e77c2697a4a1649118f9a195
Martins Silins <kydzoster@gmail.com>
2020-04-21 14:50:52 +0000

    created a route to each content using id as part of the rout

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,31 +3,17 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-# post as a blogpost with dictionaries and entrances
-posts = [
-    {
-        'author': 'Martin Silins',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2020'
-    },
-    {
-        'author': 'Kate Backingsale',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2020'
-    }
-]
 
 # this is a default home page
 @app.route("/")
 # this is a designated home page
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
 
@@ -112,3 +98,22 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
