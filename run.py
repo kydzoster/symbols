859ebaa9ec7972f330a416dd050d4e2a5d3077b7
Martins Silins <kydzoster@gmail.com>
2020-04-23 09:37:25 +0000

    added delete functions with pop up messages for delete

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from flaskblog import app
 
-# lets you run code in localhost and see every change you make in real time made in this file app.py
+# lets you run code in debug mode
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
