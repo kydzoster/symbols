77c5c1cb49c2add1f4850bb365a919288c5bec6f
Martins Silins <kydzoster@gmail.com>
2020-04-23 10:26:49 +0000

    added pagination for all entries with descending order

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -13,7 +13,10 @@ from flask_login import login_user, current_user, logout_user, login_required
 # this is a designated home page
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    # this will order posts in descending order, meaning, latest/newest posts will be displayed first,
+    # oldest will be last with 5 posts per page
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("home.html", posts=posts)
 
 
@@ -148,3 +151,16 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+# This will get all posts for a specific user
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    # this will order posts in descending order, meaning, latest/newest posts will be displayed first,
+    # oldest will be last with 5 posts per page
+    posts = Post.query\
+        .filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template("user_posts.html", posts=posts, user=user)
