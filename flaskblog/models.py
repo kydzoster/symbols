749558223888153b59cbd84314d34f2be88d555f
Martins Silins <kydzoster@gmail.com>
2020-04-27 12:45:21 +0000

    added flask-mail for password reset

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+# line below is for pasword recovery
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -14,6 +16,21 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+    # this is expiration to 30min
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id':self.id}).decode('utf-8')
+
+    # this code block verifies token
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+    
     # this will show how the object is printed when it is printed out
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
