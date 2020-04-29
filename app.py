f92329cbf3cea63f924ac493905859937357c46c
Martins Silins <kydzoster@gmail.com>
2020-04-29 15:44:55 +0000

    added category for countries and respective descriptions tab for symbols

--- app.py
+++ app.py
@@ -1,12 +1,67 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'project'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@my1stcluster-phyn3.mongodb.net/project?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/symbols')
+def symbols():
+    return render_template("symbols.html", symbols=mongo.db.symbols.find())
+
+
+@app.route('/add_symbol')
+def add_symbol():
+    return render_template('add_symbol.html',
+    # This line will find all Countries in MongoDB
+    categories=mongo.db.categories.find())
+
+# This function will deliver inserted data to MongoDB
+@app.route('/insert_symbol', methods=['POST'])
+def insert_symbol():
+    symbols = mongo.db.symbols
+    # this line will insert one form into MongoDB as a dictionary
+    symbols.insert_one(request.form.to_dict())
+    # this line after submiting will return to symbols.html
+    return redirect(url_for('symbols'))
+
+
+# This function will find properties for symbols by its id with the help of ObjectId
+@app.route('/edit_symbol/<symbol_id>')
+def edit_symbol(symbol_id):
+    the_symbol = mongo.db.symbols.find_one({'_id': ObjectId(symbol_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edit_symbol.html', symbol=the_symbol, categories=all_categories)
+
+
+@app.route('/update_symbol/<symbol_id>', methods=["POST"])
+def update_symbol(symbol_id):
+    symbols = mongo.db.symbols
+    symbols.update({'_id': ObjectId(symbol_id)},
+    {
+        'category_name':request.form.get('category_name'),
+        'symbol_name':request.form.get('symbol_name'),
+        'symbol_description': request.form.get('symbol_description')
+    })
+    return redirect(url_for('symbols'))
+
+
+@app.route('/delete_symbol/<symbol_id>')
+def delete_symbol(symbol_id):
+    mongo.db.symbols.remove({'_id': ObjectId(symbol_id)})
+    return redirect(url_for('symbols'))
+
+
+@app.route('/categories')
+def categories():
+    return render_template('categories.html',
+    categories=mongo.db.categories.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
